bb8e2aebb22c6dd262dc2550f7a9296ddfc6e0ba
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2022-02-08 13:34:39 +0100

    Warn and return if Add-Type does not work

--- scripts/broadcast-environment-change.ps1
+++ scripts/broadcast-environment-change.ps1
@@ -14,15 +14,22 @@ public static extern int SendMessageTimeout(
     out int lpdwResult
 );
 "@
-[void](
-    Add-Type -Namespace Win32Api -Name NativeMethods `
-        -MemberDefinition $SendMessageFuncDef -Language CSharp
-)
+try {
+    [void](
+        Add-Type -Namespace Win32Api -Name NativeMethods `
+            -MemberDefinition $SendMessageFuncDef -Language CSharp `
+    )
+}
+catch {
+    Write-Warning "Unable to create P/Invoke for WinApi function SendMessageTimeout"
+    Write-Warning $Error[0].Exception.Message
+    return
+}
 
 $HWND_BROADCAST = [System.IntPtr]0xFFFF
-[uint]$WM_SETTINGCHANGE = 0x001A # Same as WM_WININICHANGE
-[uint]$fuFlags = 2  # SMTO_ABORTIFHUNG: return if receiving thread does not respond (hangs)
-[uint]$timeOutMs = 1000  # Timeout in milliseconds
+[System.UInt32]$WM_SETTINGCHANGE = 0x001A # Same as WM_WININICHANGE
+[System.UInt32]$fuFlags = 2  # SMTO_ABORTIFHUNG: return if receiving thread does not respond (hangs)
+[System.UInt32]$timeOutMs = 1000  # Timeout in milliseconds
 [int]$ErrorCode = 0
 
 $ReturnValue = [Win32Api.NativeMethods]::SendMessageTimeout(
@@ -37,6 +44,7 @@ $ReturnValue = [Win32Api.NativeMethods]::SendMessageTimeout(
 $Win32Except = New-Object "System.ComponentModel.Win32Exception" $ErrorCode
 if (-not $ReturnValue) {
     throw $Win32Except
-} elseif ($VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue) {
+}
+elseif ($VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue) {
     Write-Verbose $Win32Except.Message
 }
